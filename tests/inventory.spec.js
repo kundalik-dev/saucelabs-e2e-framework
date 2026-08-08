9f730c643a9987893a0d7a44b4abf347640f0b54
kundalik-qalogs <qalogs.ai@gmail.com>
2026-08-08 13:03:55 +0530

    fix: Update product selection in addProductToCart test to use specific product name

--- tests/inventory.spec.js
+++ tests/inventory.spec.js
@@ -81,7 +81,7 @@ test.describe("Inventory test", () => {
 
   test("should add a product to the cart", async ({ page }) => {
     const user = users.valid.standard_user;
-    const products = inventoryData.productData.Backpack;
+    const products = inventoryData.productData;
 
     loginPage = new LoginPage(page);
     inventoryPage = new InventoryPage(page);
@@ -89,7 +89,7 @@ test.describe("Inventory test", () => {
     await loginPage.goto("/");
     await loginPage.login(user);
 
-    await inventoryPage.addProductToCart(products.name).click();
+    await inventoryPage.addProductToCart(products.Backpack.name).click();
 
     await expect(page).toHaveTitle("Swag Labs");
   });
